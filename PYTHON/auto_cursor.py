e46791f3e01994ff023eb3ab9c6e26f6d64aff90
anushvst <anush.1801232@gmail.com>
2024-02-23 15:06:31 +0530

    v1

---
+++ PYTHON/auto_cursor.py
@@ -0,0 +1,47 @@
+import pyautogui
+j=3
+import time
+while True:
+    try:
+        x,y=pyautogui.position()
+        pyautogui.scroll(j)
+        pyautogui.press('x')
+        pyautogui.press('o')
+        pyautogui.press('t')
+        pyautogui.press('i')
+        pyautogui.press('l')
+        pyautogui.press('l')
+        pyautogui.press('w')
+        pyautogui.press('e')
+        pyautogui.press('o')
+        pyautogui.press('d')
+        pyautogui.press(':')
+        pyautogui.press(')')
+                
+        time.sleep(10)
+        pyautogui.keyDown('alt')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.keyUp('alt')
+        
+        time.sleep(10)
+        pyautogui.scroll(j)
+        pyautogui.press('y')
+        pyautogui.press('e')
+        pyautogui.press('a')
+        pyautogui.press('h')
+        pyautogui.press('t')
+        pyautogui.press('h')
+        pyautogui.press('a')
+        pyautogui.press('t')
+        pyautogui.press('s')
+        pyautogui.press('i')
+        pyautogui.press('t')
+        pyautogui.press('!')
+        xx, yy=pyautogui.position()
+
+    except:
+        pass
